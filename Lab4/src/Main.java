848c6ad7a5e89b8fe0713eae8e875288b8e699fb
Jonathan Lopez <jonathan.lopez.1224@gmail.com>
2020-11-19 00:37:24 -0500

    Add files via upload

---
+++ Lab4/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+        VoteBooth aBooth = new VoteBooth();
+
+
+        aBooth.screen();
+
+
+
+
+        // what to do on startup goes here
+
+    }
+
+
+
+
+
+
+
+
+
+}
